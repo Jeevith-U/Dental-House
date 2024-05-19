488664a9aa2d0bd5571e8b27ba8ad14881f747fd
rajumb0232 <rajugowda0232@gmail.com>
2024-05-19 16:08:44 +0530

    project initialized

---
+++ Dental-House/src/test/java/com/devbrain/dentahouse/DentalHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbrain.dentahouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
