1926e4e48afc4a5fead5aa1adb79c589b6f5ef95
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2024-05-28 23:26:28 +0530

    Added Image Need correction

--- Dental-House-UI/src/Components/LandingPage/Landingpage.jsx
+++ Dental-House-UI/src/Components/LandingPage/Landingpage.jsx
@@ -5,7 +5,7 @@ const Landingpage = () => {
     <div>
       <div className="background bg-custom-gradient min-h-screen">
         <div className="container">
-            <div className="image flex-1 lg:ml-4 p-4 w-13 ">
+            <div className="image w-full lg:w-2/5 p-4">
                 <div className="bg-white p-4 rounded-lg shadow-lg flex items-end justify-center">
                     <img src = {doctorimg} alt='Doctor-img' className= ""/>
                 </div>
