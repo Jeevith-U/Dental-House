bda0412d6ad45d07893156aa87f4c373a1c1fed0
rajumb0232 <rajugowda0232@gmail.com>
2024-06-11 06:43:01 +0530

    added missing all args constructor

--- Dental-House/src/main/java/com/devbrain/dentahouse/security/UserDetailServiceImpl.java
+++ Dental-House/src/main/java/com/devbrain/dentahouse/security/UserDetailServiceImpl.java
@@ -1,12 +1,14 @@
 package com.devbrain.dentahouse.security;
 
 import com.devbrain.dentahouse.repository.DoctorRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class UserDetailServiceImpl implements UserDetailsService {
 
     private DoctorRepository doctorRepository;
