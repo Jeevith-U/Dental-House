4a062db29240893b0808db160b118621af283c1b
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2024-06-16 13:13:21 +0530

    Resolving Overflow issues

--- Dental-House-UI/src/Components/Forms/Forms.jsx
+++ Dental-House-UI/src/Components/Forms/Forms.jsx
@@ -118,7 +118,7 @@ const Forms = () => {
                 <div className="flex items-center justify-end">
                   <button
                     type="submit"
-                    className="mt-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    className="mt-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" 
                   >
                     Submit
                   </button>
