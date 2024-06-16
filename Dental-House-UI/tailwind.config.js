cf26f242810b560c02041aaa5baf188ec2639498
rajumb0232 <rajugowda0232@gmail.com>
2024-06-16 18:45:46 +0530

    added google fonts

--- Dental-House-UI/tailwind.config.js
+++ Dental-House-UI/tailwind.config.js
@@ -11,8 +11,16 @@ export default {
         'custom-gradient': 'linear-gradient(0deg, #D1A1F7 0%, #F5F5F5 23%, #D8D8D8 59%, #30526D 100%)'
       },
       colors:{
-        "blue_gray" : "30526D",
+        "blue_gray": "30526D",
         "bg-blue": "#D1E5F0"
+      },
+      fontFamily:{
+        "great-vibes": ["Great Vibes", "cursive"],
+        "cormorant-garamond": ["Cormorant Garamond", "serif"],
+        "caveat": ["Caveat", "cursive"],
+        "open-sans": ["Open Sans", "sans-serif"],
+        "poppins": ["Poppins", "sans-serif"],
+        "comfortaa": ["Comfortaa", "sans-serif"]
       }
     },
   },
